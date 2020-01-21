525ce030dfcc1c3eb557d588e50d400ac25e60a8
Rory McCann <rory@technomancy.org>
2020-01-21 18:42:14 +0100

    record when it starts so we can make eta calculations

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 use std::path::PathBuf;
 use std::io::Read;
 use std::fs::File;
+use std::time::{Instant, Duration};
 
 /// A wrapper for a `Read` that monitors how many bytes have been read, and how many are to go
 pub struct ReaderWithSize<R: Read> {
@@ -9,12 +10,13 @@ pub struct ReaderWithSize<R: Read> {
 
     total_size: usize,
     total_read: usize,
+    read_start_time: Option<Instant>,
 }
 
 impl<R: Read> ReaderWithSize<R> {
     /// Create a ReaderWithSize from `inner` presuming the total number of bytes is `total_size`.
     pub fn new(total_size: usize, inner: R) -> Self {
-        ReaderWithSize{ total_size, total_read: 0, inner }
+        ReaderWithSize{ total_size, total_read: 0, inner, read_start_time: None }
     }
 
     /// The total number of bytes that have been read from this reader
@@ -42,6 +44,38 @@ impl<R: Read> ReaderWithSize<R> {
     /// A reference to the inner `Read`.
     pub fn inner(&self) -> &R {
         &self.inner
+    }
+
+    pub fn read_start_time(&self) -> Option<Instant> {
+        self.read_start_time
+    }
+
+    pub fn eta(&self) -> Option<Duration> {
+        self.read_start_time.map(|read_start_time| {
+            let duration_since_start = Instant::now() - read_start_time;
+            duration_since_start.div_f64(self.fraction()) - duration_since_start
+        })
+    }
+
+    pub fn est_total_time(&self) -> Option<Duration> {
+        self.read_start_time.map(|read_start_time| {
+            let duration_since_start = Instant::now() - read_start_time;
+            duration_since_start.div_f64(self.fraction())
+        })
+    }
+
+    pub fn etc(&self) -> Option<Instant> {
+        self.read_start_time.map(|read_start_time| {
+            let duration_since_start = Instant::now() - read_start_time;
+            read_start_time + duration_since_start.div_f64(self.fraction())
+        })
+    }
+
+    pub fn bytes_per_sec(&self) -> Option<f64> {
+        self.read_start_time.map(|read_start_time| {
+            let since_start = Instant::now() - read_start_time;
+            (self.total_read as f64)/since_start.as_secs_f64()
+        })
     }
 
 }
